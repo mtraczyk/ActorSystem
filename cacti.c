b422d20ed702bcf8cbfff0a11eb17c0a269b2d52
Michał Traczyk <mt418401@students.mimuw.edu.pl>
2021-01-26 22:39:24 +0100

    Allocing and reallocing memory.

--- cacti.c
+++ cacti.c
@@ -44,6 +44,9 @@ static actor_id_t **queue_of_actors;
 // Current size of each queue_of_actors array.
 static int *queue_of_actors_length;
 
+// Current number of actors in each of the queues.
+static int *number_of_actors_in_queue;
+
 /* begin_cyclic_actors_index, tells a thread
  * which actor should receive a message.
  *
@@ -90,31 +93,74 @@ static void initialize_system_global_memory(actor_id_t *actor, role_t *const rol
 
   check_alloc_validity(actor_messages = malloc(sizeof(message_t *)));
   check_alloc_validity(actor_messages[0] = malloc(ACTOR_QUEUE_LIMIT * sizeof(message_t)));
-  check_alloc_validity(begin_cyclic_messages_index = malloc(sizeof(int)));
-  check_alloc_validity(end_cyclic_messages_index = malloc(sizeof(int)));
+  check_alloc_validity(begin_cyclic_messages_index = calloc(1, sizeof(int)));
+  check_alloc_validity(end_cyclic_messages_index = calloc(1, sizeof(int)));
 
   check_alloc_validity(queue_of_actors = malloc(POOL_SIZE * sizeof(actor_id_t *)));
   check_alloc_validity(queue_of_actors_length = malloc(POOL_SIZE * sizeof(int)));
-  check_alloc_validity(begin_cyclic_actors_index = malloc(POOL_SIZE * sizeof(int)));
-  check_alloc_validity(end_cyclic_actors_index = malloc(POOL_SIZE * sizeof(int)));
+  check_alloc_validity(begin_cyclic_actors_index = calloc(POOL_SIZE, sizeof(int)));
+  check_alloc_validity(end_cyclic_actors_index = calloc(POOL_SIZE, sizeof(int)));
+  check_alloc_validity(queue_of_actors_length = calloc(POOL_SIZE, sizeof(int)));
+  check_alloc_validity(number_of_actors_in_queue = calloc(POOL_SIZE, sizeof(int)));
 
   for (int i = 0; i < POOL_SIZE; i++)
     check_alloc_validity(queue_of_actors[i] = malloc(sizeof(actor_id_t)));
 
-  begin_cyclic_messages_index = end_cyclic_messages_index = 0;
-  for (int i = 0; i < POOL_SIZE; i++) {
+  for (int i = 0; i < POOL_SIZE; i++)
     queue_of_actors_length[i] = 1;
-    begin_cyclic_actors_index[i] = 0;
-    end_cyclic_actors_index[i] = 0;
-  }
 }
 
+// Adjusting length of actor_roles and actor_messages.
 static void adjust_dynamic_arrays() {
+  // I`ve got to acquire the mutex.
+  int s = pthread_mutex_lock(&mutex);
+  if (s != 0)
+    handle_error_en(s, "pthread_mutex");
 
+  if (actor_roles_and_messages_length == number_of_actors) {
+    actor_roles_and_messages_length = actor_roles_and_messages_length * MULTIPLIER / DIVIDER;
+    check_alloc_validity(actor_roles =
+                           realloc(actor_roles,
+                                   actor_roles_and_messages_length * sizeof(role_t *)));
+    check_alloc_validity(actor_messages =
+                           realloc(actor_messages,
+                                   actor_roles_and_messages_length * sizeof(message_t *)));
+    check_alloc_validity(begin_cyclic_messages_index =
+                           realloc(begin_cyclic_messages_index,
+                                   actor_roles_and_messages_length * sizeof(int)));
+    check_alloc_validity(end_cyclic_messages_index =
+                           realloc(end_cyclic_messages_index,
+                                   actor_roles_and_messages_length * sizeof(int)));
+
+    for (int i = number_of_actors; i < actor_roles_and_messages_length; i++) {
+      check_alloc_validity(actor_messages[i] = malloc(ACTOR_QUEUE_LIMIT * sizeof(message_t)));
+      begin_cyclic_messages_index[i] = 0;
+      end_cyclic_messages_index[i] = 0;
+    }
+  }
+
+  s = pthread_mutex_unlock(&mutex);
+  if (s != 0)
+    handle_error_en(s, "pthread_mutex");
 }
 
-static void adjust_queue_of_actors() {
+static void adjust_queue_of_actors(int queue_number) {
+  // I`ve got to acquire the mutex.
+  int s = pthread_mutex_lock(&mutex);
+  if (s != 0)
+    handle_error_en(s, "pthread_mutex");
 
+  if (queue_of_actors_length[queue_number] == number_of_actors_in_queue[queue_number]) {
+    queue_of_actors_length[queue_number] =
+      queue_of_actors_length[queue_number] * MULTIPLIER / DIVIDER;
+    check_alloc_validity(queue_of_actors[queue_number] =
+                           realloc(queue_of_actors[queue_number],
+                                   queue_of_actors_length[queue_number] * sizeof(actor_id_t)));
+  }
+
+  s = pthread_mutex_unlock(&mutex);
+  if (s != 0)
+    handle_error_en(s, "pthread_mutex");
 }
 
 actor_id_t actor_id_self() {
