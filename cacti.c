073762c02bb61f7b61109e6ef969a1d4a23e034c
Michał Traczyk <mt418401@students.mimuw.edu.pl>
2021-01-26 20:22:47 +0100

    Even more variables.

--- cacti.c
+++ cacti.c
@@ -7,36 +7,36 @@
 // Global data for the current system.
 
 // Number of actors in the current system.
-actor_id_t number_of_actors = 0;
+static actor_id_t number_of_actors = 0;
 
 // Array of actors` roles.
-role_t *actor_roles;
+static role_t *actor_roles;
 
 // Current size of actor_roles array.
-int actor_roles_length = 0;
+static int actor_roles_length = 0;
 
 /* Cyclic buffer of actors` messages.
  * Acts as a queue to be more cache friendly.
 */
-message_t **actor_messages;
+static message_t **actor_messages;
 
 // Current size of actor_messages array.
-int actor_messages_length = 0;
+static int actor_messages_length = 0;
 
 /* begin_cyclic_messages_index, tells an actor which message should be received.
  *
  * end_cyclic_messages_index, tells the system where to save a new message.
 */
-int *begin_cyclic_messages_index, *end_cyclic_messages_index;
+static int *begin_cyclic_messages_index, *end_cyclic_messages_index;
 
 /* Cyclic buffer of actors that have a message to receive.
  * One for every thread. Processing actors in a queue will guarantee liveness
  * of the system.
 */
-actor_id_t **queue_of_actors;
+static actor_id_t **queue_of_actors;
 
-// Current size of every queue_of_actors array.
-int *queue_of_actors_length;
+// Current size of each queue_of_actors array.
+static int *queue_of_actors_length;
 
 /* begin_cyclic_actors_index, tells a thread
  * which actor should receive a message.
@@ -44,8 +44,16 @@ int *queue_of_actors_length;
  * end_cyclic_actors_index, tells a thread where to save info
  * about an actor awaiting a message.
 */
-int *begin_cyclic_actors_index, *end_cyclic_actors_index;
+static int *begin_cyclic_actors_index, *end_cyclic_actors_index;
 
+// Multiplier for reallocs in implementation of a vector.
+static const int multiplier = 3;
+
+// Divider for reallocs in implementation of a vector.
+static const int divider = 2;
+
+
+// Macros
 
 // Macro for error handlers.
 #define handle_error_en(en, msg) \
