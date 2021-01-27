ea69a4f37932e524336f6d7bb14cbf52b4b1bb64
Michał Traczyk <mt418401@students.mimuw.edu.pl>
2021-01-27 14:51:42 +0100

    Added needed structures.

--- global.h
+++ global.h
@@ -8,8 +8,36 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-extern role_t **actor_role; // Array of pointers to actors roles.
+extern uint32_t number_of_actors; // Number of actors in the system.
 
+// Cyclic buffer of messages acting as a queue.
+typedef struct message_buffer {
+  message_t *messages; // The actual data.
+  int readpos, writepos; // Positions for reading and writing.
+  int size; // Number of messages in the messages array.
+} message_buffer;
+
+// Basic info about an actor.
+typedef struct actor_info {
+  actor_id_t id; // Actor`s id.
+  role_t *role; // Actor`s role.
+  void *state; // Actor`s state.
+  message_buffer msg_q; // Buffer of messages acting as a queue.
+  pthread_mutex_t lock;  // Mutex ensuring exclusive access to buffer.
+} actor_info;
+
+extern actor_info *actors; // An array of actors` info.
+extern uint32_t actor_info_length; // Length of an actors array.
+
+// Buffer of actor_id_t acting as a queue.
+typedef struct actor_buffer {
+  actor_id_t *actor_id; // Actor`s id.
+  pthread_mutex_t lock;  // Mutex ensuring exclusive access to buffer.
+  int readpos, writepos; // Positions for reading and writing.
+  int size; // Size of the buffer.
+} actor_buffer;
+
+extern actor_buffer *actor_q[POOL_SIZE]; // Actor_buffer, one for every thread acting as a queue.
 
 
 // Constants
